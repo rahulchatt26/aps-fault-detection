32d72a6078845460c801597a0197fb30fdb2d9b2
Rahul Chatterjee <chatterjeerahul2604@gmail.com>
2022-12-01 15:25:52 +0530

    config env file added

---
+++ sensor/config.py
@@ -0,0 +1,13 @@
+import pymongo
+import pandas as pd
+import json
+from dataclasses import dataclass
+import os
+
+# Provide the mongodb localhost url to connect python to mongodb.
+@dataclass
+class GetEnvironmentVariable:
+    mongo_db_url:str = os.getenv("MONGO_DB_URL")
+
+env_var = GetEnvironmentVariable()
+mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
