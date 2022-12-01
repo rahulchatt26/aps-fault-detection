40319df57f7ee3889f1541f462feabd6ac1d2c51
Rahul Chatterjee <chatterjeerahul2604@gmail.com>
2022-12-01 17:06:11 +0530

    first component Data Ingestion

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ from sensor.logger import logging
 from sensor.exception import SensorException
 import sys,os
 from sensor.utils import get_collection_as_dataframe
+from sensor.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig
+from sensor.components.data_ingestion import DataIngestion
 
 
 if __name__=="__main__":
      try:
-          get_collection_as_dataframe(database_name="aps", collection_name="sensor")
+          training_pipeline_config = TrainingPipelineConfig()
+          data_ingestion_config = DataIngestionConfig(training_pipeline_config)
+          print(data_ingestion_config.to_dict())
+          data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+          print(data_ingestion.initiate_data_ingestion())
+
      except Exception as e:
           print(e)
--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -1,6 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionArtifact:
+    feature_store_filepath:str
+    train_file_path:str
+    test_file_path:str
 
 
-class DataIngestionArtifact:...
 class DataValidationArtiface:...
 class DataTransformationArtiface:...
 class ModelTrainerArtifact:...
